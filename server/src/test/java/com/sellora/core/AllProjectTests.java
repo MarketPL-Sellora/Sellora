d955d44632b001b971487d71d66bfda05ee78514
cnnbless <antongmain@gmail.com>
2026-06-05 19:34:16 +0300

    test: added new unit tests to All tests file

--- server/src/test/java/com/sellora/core/AllProjectTests.java
+++ server/src/test/java/com/sellora/core/AllProjectTests.java
@@ -1,25 +1,35 @@
-//package com.sellora.core;
-//
-//import com.sellora.core.application.usecases.*;
-//import org.junit.platform.suite.api.SelectClasses;
-//import org.junit.platform.suite.api.Suite;
-//
-//@Suite
-//@SelectClasses({
-//  AuthServiceTest.class,
-//  AuthControllerTest.class,
-//  CategoryServiceTest.class,
-//  GlobalExceptionHandlerTest.class,
-//  GroupBuySessionControllerTest.class,
-//  GroupBuySessionServiceTest.class,
-//  JwtAuthenticationFilterTest.class,
-//  JwtServiceTest.class,
-//  ProductControllerTest.class,
-//  ProductServiceTest.class,
-//  ProductSpecificationTest.class,
-//  StoreServiceTest.class
-//
-//})
-//public class AllProjectTests {
-//  // Тіло класу залишається порожнім
-//}
+package com.sellora.core;
+
+import com.sellora.core.application.usecases.*;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({
+  AuthServiceTest.class,
+  AuthControllerTest.class,
+  CategoryServiceTest.class,
+  GlobalExceptionHandlerTest.class,
+  GroupBuySessionControllerTest.class,
+  GroupBuySessionServiceTest.class,
+  JwtAuthenticationFilterTest.class,
+  JwtServiceTest.class,
+  ProductControllerTest.class,
+  ProductServiceTest.class,
+  ProductSpecificationTest.class,
+  StoreServiceTest.class,
+  CartServiceTest.class,
+  CartControllerTest.class,
+  FavoriteControllerTest.class,
+  UserFavoriteServiceTest.class,
+  MerchantRequisiteControllerTest.class,
+  MerchantRequisiteServiceTest.class,
+  PromoCodeControllerTest.class,
+  ShippingCarrierControllerTest.class,
+  UserSettingsControllerTest.class,
+  UserSettingsServiceTest.class,
+  UserControllerTest.class,
+})
+public class AllProjectTests {
+
+}
